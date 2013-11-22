81bf5c0316f5ef3713d7f230c62ec8fa22c3fac3
Michael DeGuzis <mdeguzis@gmail.com>
2013-11-21 22:04:13 -0500

    added scripts and folder

---
+++ jb_live_scripts/jb_live_planb.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+#Script to record JB Live shows
+#
+cd /home/$USER/Videos
+ffmpeg -i rtsp://videocdn-us.geocdn.scaleengine.net/jblive/live/jblive.stream -b 900k -vcodec copy -r 60 -t 02:00:00 -y planb+`date +%Y%m%d`.avi
+cd /home/$USER
+
