7570009f79c98dc6f98561e82eb15aab7912fdb1
Michael DeGuzis <mdeguzis@gmail.com>
2014-05-16 07:06:41 -0400

    added repo clean scripts!

--- jb_live_scripts/ffmpeg/jb_live_techsnap.sh
+++ jb_live_scripts/ffmpeg/jb_live_techsnap.sh
@@ -2,7 +2,7 @@
 #
 #Script to record JB Live shows
 #
-cd /home/$USER/Videos
+cd $HOME/Videos
 ffmpeg -f rtsp -rtsp_transport tcp -i rtsp://videocdn-us.geocdn.scaleengine.net/jblive/live/jblive.stream -strict -2 -vcodec copy -t 02:00:00 -y techsnap_`date +%Y%m%d`.avi
-cd /home/$USER
+cd $HOME
 
