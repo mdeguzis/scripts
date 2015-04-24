e185c02b0b22970394787ad424780e184a8c7f4d
Michael DeGuzis <mdeguzis@gmail.com>
2015-04-24 14:58:06 -0400

    Update search-mame.sh

--- search-mame.sh
+++ search-mame.sh
@@ -25,7 +25,7 @@ main()
   mametxt="$scriptdir/extra/MAME.txt"
   
   # Search game list
-  gameresults_title=$(grep -E $gamearg $mametxt | grep -E 'Game: ' | grep -E 'Game Filename: ' |) 
+  gameresults_title=$(grep -E $gamearg $mametxt | grep -E 'Game: ' | grep -E 'Game Filename: ') 
   gameresults_file=$(grep -i $gamearg $mametxt | grep -i "Game Filename: " | xargs 
   Game Title: ')
   
