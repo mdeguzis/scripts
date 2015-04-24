5821dd9d0b884e0ffbbebbf87713654aa4bb2ab5
Michael DeGuzis <mdeguzis@gmail.com>
2015-04-24 13:51:58 -0400

    Update search-mame.sh

--- search-mame.sh
+++ search-mame.sh
@@ -1,2 +1,39 @@
-!#/bin/bash
+#!/bin/bash
 
+# -------------------------------------------------------------------------------
+# Author:     	Michael DeGuzis
+# Git:	      	https://github.com/ProfessorKaos64/SteamOS-Tools
+# Scipt Name: 	build-test-chroot.sh
+# Script Ver: 	0.1.1
+# Description:	Searches latest available mame.txt for game, returns file
+#               name matches to look for in your list of ROMs.
+#	
+# -------------------------------------------------------------------------------
+
+# set vars
+scriptdir=(pwd)
+mametxt="$scriptdir/extra/MAME.txt"
+gamearg="$1"
+
+main()
+{
+  
+  # obtain latest mame list
+  # TODO
+  
+  # Use listing I have from RetroRig-ES for now
+  mametxt="$scriptdir/extra/MAME.txt"
+  
+  # Search game list
+  gameresults=(grep -i $gamearg)
+  
+  # Format results
+  TODO
+  
+  # echo output
+  echo -e "\nMatches that closely relate to the game title ${gamearg}:\n"
+
+}
+
+# Start main
+main
