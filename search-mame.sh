1c3508a7f695fcdaf4d65b2f5cd935ee49ee2da4
Michael DeGuzis <mdeguzis@gmail.com>
2015-04-24 15:29:18 -0400

    Update search-mame.sh

--- search-mame.sh
+++ search-mame.sh
@@ -25,10 +25,11 @@ main()
   mametxt="$scriptdir/extra/MAME.txt"
   
   # Search game list
-  gameresults_title=$(grep -i $gamearg $mametxt | grep -i -A 4 "Game: ")
-  gameresults_file=$(grep -i $gamearg $mametxt | grep -i "Game Filename: ")
+  gameresults_title=$(grep -i $gamearg $mametxt | grep -i "Game: ")
+  gameresults_file=$(grep -i $gamearg $mametxt | grep -i -A 5 "Game: " | xargs grep -i "Game Filename: ")
   
-  echo "$gameresults_title"
+ # echo "$gameresults_title"
+  echo "$gameresults_file"
   exit
   
   if [[ "$gameresults_title" == "" ]]; then
