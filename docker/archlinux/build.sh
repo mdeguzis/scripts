5c83a93c56ba8c70a13aaccf4da864e6716e50ee
Michael DeGuzis <deguzim@amazon.com>
2024-03-06 20:54:55 +0000

    Update dockerfile

--- docker/archlinux/build.sh
+++ docker/archlinux/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+VOLUME_PATH="/tmp/target"
+echo "[INFO] Creating Docker volume path ${VOLUME_PATH}"
+mkdir -p "${VOLUME_PATH}"
+chown -R "${USER}" "${VOLUME_PATH}"
+
 echo "[INFO] Building Docker image"
 docker build . \
-	--build-arg HOST_USER="${USER}" \
+	--build-arg HOST_USER=${USER} \
+	--build-arg UID=$(id -u) \
+	--build-arg GID=$(id -g) \
 	--tag archlinux-dev
