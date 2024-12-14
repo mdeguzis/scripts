9a4b74074d5d648007944bbdccc11be5a9c69a3b
Michael DeGuzis <mdeguzis@gmail.com>
2024-12-14 14:29:19 -0500

    Verified amount on PDF vs processed

--- finance/monthly-budget.py
+++ finance/monthly-budget.py
@@ -8,7 +8,15 @@ import pandas as pd
 import pdfplumber
 import re
 
+from decimal import Decimal
 from pathlib import Path
+
+# Default json library doesn't like to serialize decimal :)
+# Convert Decimal objects to strings before serializing to JSON
+def decimal_default(obj):
+    if isinstance(obj, Decimal):
+        return str(obj)
+    raise TypeError("Object of type Decimal is not JSON serializable")
 
 def analyze_capitalone_csv(file_path):
     try:
@@ -49,57 +57,104 @@ def parse_capitalone_transactions_text(pdf_text, data, page_num):
     lines = pdf_text.splitlines()
     current_name = None
     current_account = None
+    have_continuation = False
     transaction_ct = 0
 
-    name_pattern = re.compile(r"^([A-Z\s]+) #(\d+): Transactions$")
-    user_transactions_done = re.compile(r"^([A-Z\s]+) #(\d+): Total Transactions")
-    transaction_pattern = re.compile(r"(\w{3} \d{1,2}) (\w{3} \d{1,2}) ([\w\s\*]+.*?[a-zA-Z]) (\$\d+\.\d{1,2})")
+    name_pattern = re.compile(r"^([A-Z\s]+) #(\d+): Transactions")
+    user_transactions_done = re.compile(r"^([A-Z\s]+) #(\d+): Total Transactions (\$\d{1,3}(?:,\d{3})*(?:\.\d{1,2}))")
+    # Match any variation of money + expected pattern
+    # Example: 'Nov 5 Nov 8 Moble Payment - ABCD $0,000.00"
+    # Example: 'Nov 22 Nov 22 Moble Payment (new) - ABCD $0.00"
+    transaction_pattern = re.compile(r"(\w{3} \d{1,2}) (\w{3} \d{1,2}) ([\w\s\*]+.*?[a-zA-Z]) (\$\d{1,3}(?:,\d{3})*(?:\.\d{1,2}))")
     header = "Trans Date Post Date Description Amount"
     processing_transactions = False
 
     for line in lines:
-        match = name_pattern.match(line)
+        # Fetch current user from queue memory
+        # Regex matching
+        if data.get("current_queue", ""):
+            current_name = data["current_queue"] 
+        name_match = name_pattern.match(line)
+        done_match = user_transactions_done.match(line)
+
+        # If we hit "<NAME> #<ACCOUNT>: Total Transactions", we are done for the current user
+        #if done_match and not processing_transactions:
+        # Check this early, as if we trigger done too early, we'll notice discrepancies
+        if done_match:
+            if have_continuation:
+                logger.error("We should still be processing transactions. Something went wrong...")
+                exit(1)
+            this_user = done_match.group(1)
+            logger.info(f"Done processing transactions for '{this_user}'")
+            # Our total
+            total_amount_from_data = data[this_user]["transactions_total_amount"]
+            logger.debug(f"Converting our total {total_amount_from_data} to Decimal")
+            total_amount_processed = Decimal(total_amount_from_data)
+            # Statement total
+            statement_final = done_match.group(3).replace('$', '').replace(',', '')
+            logger.debug(f"Converting statement final total {statement_final} to Decimal")
+            statement_final_amount = Decimal(statement_final)
+            # Verify
+            if total_amount_processed != statement_final_amount:
+                logger.error(f"Failed to verify transaction amounts against process amount!")
+                logger.error(f"Found: {total_amount_processed}")
+                logger.error(f"Reported by document: {statement_final_amount}")
+                #exit(1)
+            logger.info("Final ammount verified!")
+            data[this_user]["verified_amounts"] = True
+
+            # Error if our current total amoutn processed doesn't match the amoutn reported in the final line
+            # Clear the queue
+            data["current_queue"] = None
+            processing_transactions = False
+            break
+
         # Get current person we are handling
-        if match and not processing_transactions:
-            current_name = match.group(1)  # Name part up to '#1234'
-            current_account = match.group(2)  # The account number
-            logger.info(f"Processing transaction for '{current_name}' (Account #{current_account})")
+        if name_match:
+            current_name = name_match.group(1)
+            current_account = name_match.group(2)
+            # Set name in the queue so we have it handy
+            data["current_queue"] = current_name
+            logger.info(f"Processing transactions for '{current_name}' (Account #{current_account})")
 
             # Initialize if we don't have data yet
             if current_name not in data:
                 data[current_name] = {}
                 data[current_name]["account"] = current_account
 
+            if not data.get(current_name, "").get("verified_amounts", ""):
+                data[current_name]["verified_amounts"] = False
             if not data.get(current_name, "").get("transactions_count", ""):
                 data[current_name]["transactions_count"] = []
-
             if not data.get(current_name, "").get("transactions", ""):
                 data[current_name]["transactions"] = []
-
+            if not data.get(current_name, "").get("transactions_total_amount", ""):
+                data[current_name]["transactions_total_amount"] = Decimal(0)
             processing_transactions = False
-            continue
 
-        if line == header and current_name and not processing_transactions:
-            logger.info(f"Got transactions on page {page_num}")
-            # Initialize transactions data
-            if not data.get(current_name, "").get("transactions", ""):
-                data[current_name]["transactions"] = []
-            # Set flow
+        elif "Transactions (Continued)" in line:
+            logger.info(f"We have more transactions on page {page_num}: (Continuation found)")
             processing_transactions = True
+            have_continuation = True
             continue
 
-        # If we hit "<NAME> #<ACCOUNT>: Total Transactions", we are done for the current user
-        done_match = user_transactions_done.match(line)
-        if done_match:
-            this_user = (done_match.group(1))
-            logger.info(f"Done processing transactions for '{this_user}'")
-            processing_transactions = False
+        elif line == header and current_name:
+            processing_transactions = True
+            have_continuation = False
+            # Do we have a transactions header and are we processing a user?
+            logger.debug(f"Got transactions on page {page_num}")
+            if not data.get(current_name, "").get("transactions", ""):
+                data[current_name]["transactions"] = []
+            continue
 
-        # Process transactions list
-        # "Trans Date Post Date Description Amount"
-        if current_name:
+        elif current_name and processing_transactions:
+            # Do we have an active queue and should process transactions?
+            # Header: "'Trans Date' 'Post Date' 'Description' 'Amount'"
+            have_continuation = False
             try:
                 data_match = transaction_pattern.match(line)
+                if not data_match:
+                    logger.debug(f"Discarding possible transaction line: {line}")
                 transactions_data_raw = line
                 logger.debug(f"Transaction data (raw): '{transactions_data_raw}'")
                 if data_match:
@@ -119,6 +174,14 @@ def parse_capitalone_transactions_text(pdf_text, data, page_num):
                             "amount": amount
                         }
                     )
+                    # Ensure that all the transactions we collect match up later to
+                    # the total amount
+                    logger.debug(f"Coverting {amount} to Decimal")
+                    amount_decimal = Decimal(amount.replace(',', ''))
+                    logger.debug(f"Addding {amount_decimal} to total")
+                    data[current_name]["transactions_total_amount"] += amount_decimal
+                    total = data[current_name]["transactions_total_amount"]
+                    logger.debug(f"Total: {total}")
             except Exception as e:
                 raise Exception(e)
 
@@ -150,11 +213,11 @@ def main(args, report_filename):
 
     # Print data
     if args.print:
-        logger.info(json.dumps(transaction_data, indent=4))
+        logger.info(json.dumps(transaction_data, indent=4, default=decimal_default))
 
     # Write report
     with open(report_filename, "w") as outfile:
-        json.dump(transaction_data, outfile, indent=4)
+        json.dump(transaction_data, outfile, indent=4, default=decimal_default)
 
 if __name__ == "__main__":
     args = process_args()
@@ -186,4 +249,5 @@ if __name__ == "__main__":
 
     print(f"Log: {log_filename}")
     print(f"Transactions report: {report_filename}")
+
 
